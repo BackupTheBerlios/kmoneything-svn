0f0e7100c282c053e6a77e636a8a88eb4469d23d
fred <fred@bcc0a2b9-e3df-0310-bd7a-c820e10b2759>
2004-07-25 20:34:05 +0000

    Added the $Id$ line, and set the properties

--- trunk/src/kmoneythingcashaccount.h
+++ trunk/src/kmoneythingcashaccount.h
@@ -17,6 +17,9 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
+
+// $Id$
+ 
 #ifndef KMONEYTHINGCASHACCOUNT_H
 #define KMONEYTHINGCASHACCOUNT_H
 
